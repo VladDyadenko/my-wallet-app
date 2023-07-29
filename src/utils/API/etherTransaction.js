a9259830c633c84f41469780e44f816b2ea3ef85
VladDyadenko <Stilmagroup@gmail.com>
2023-07-29 15:57:07 +0300

    added .env file

--- src/utils/API/etherTransaction.js
+++ src/utils/API/etherTransaction.js
@@ -4,9 +4,11 @@ import { Notify } from "notiflix";
 export const sendEther = async (amount, receiverAddress) => {
   try {
     const provider = new ethers.BrowserProvider(window.ethereum);
-    const SIGNER_PRIVATE_KEY =
-      "7ed847d4a8770a9e7bfe051e430bba64200c70124eef5626853f87266f8e4309";
-    const signer = new ethers.Wallet(SIGNER_PRIVATE_KEY, provider);
+
+    const signer = new ethers.Wallet(
+      import.meta.env.VITE_SIGNER_PRIVATE_KEY,
+      provider
+    );
     const tx = await signer.sendTransaction({
       to: receiverAddress,
       value: ethers.parseEther(amount),
